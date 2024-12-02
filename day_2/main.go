3a265fe4563f2ad38bc793bf263dec32bd9d43ad
ratataque <ewanrimmely@outlook.fr>
2024-12-02 23:38:21 +0100

    part 2

--- day_2/main.go
+++ day_2/main.go
@@ -9,25 +9,90 @@ import (
 	"time"
 )
 
+func reverse(arr []int) []int {
+	reversed := make([]int, len(arr))
+	for i, j := 0, len(arr)-1; j >= 0; i, j = i+1, j-1 {
+		reversed[i] = arr[j]
+	}
+	return reversed
+}
+
+func parseReport(line string) []int {
+	// parts := strings.Fields(line)
+	parts := strings.Split(line, " ")
+	report := make([]int, 0, len(parts))
+
+	for _, part := range parts {
+		num, err := strconv.Atoi(part)
+		if err == nil {
+			report = append(report, num)
+		}
+	}
+
+	return report
+}
+
 func isSafe(report []int) bool {
 	if len(report) < 2 {
 		return false
 	}
 
-	// Determine initial direction
+	// initial direction
 	isIncreasing := report[1] > report[0]
 
 	for i := 1; i < len(report); i++ {
 		diff := report[i] - report[i-1]
 
-		// Check if diff is within acceptable range
 		if diff == 0 || abs(diff) > 3 {
 			return false
 		}
 
-		// Check if direction is consistent
+		// check if direction is consistent
 		if (isIncreasing && diff <= 0) || (!isIncreasing && diff >= 0) {
 			return false
+		}
+	}
+
+	return true
+}
+
+func isAlmostSafe(report []int) bool {
+	if len(report) < 2 {
+		return false
+	}
+
+	// initial direction
+	isIncreasing := report[1] > report[0]
+
+	var skip bool
+	var skipped bool
+	var diff int
+	for i := 1; i < len(report); i++ {
+		if skip {
+			diff = report[i] - report[i-2]
+			skip = false
+			skipped = true
+		} else {
+			diff = report[i] - report[i-1]
+		}
+
+		if diff == 0 || abs(diff) > 3 {
+			if skipped {
+				return false
+			} else {
+				skip = true
+				continue
+			}
+		}
+
+		// check if direction is consistent
+		if (isIncreasing && diff <= 0) || (!isIncreasing && diff >= 0) {
+			if skipped {
+				return false
+			} else {
+				skip = true
+				continue
+			}
 		}
 	}
 
@@ -41,20 +106,6 @@ func abs(x int) int {
 	return x
 }
 
-func parseReport(line string) []int {
-	parts := strings.Fields(line)
-	report := make([]int, 0, len(parts))
-
-	for _, part := range parts {
-		num, err := strconv.Atoi(part)
-		if err == nil {
-			report = append(report, num)
-		}
-	}
-
-	return report
-}
-
 func main() {
 	start := time.Now()
 
@@ -63,13 +114,22 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	safeReports := 0
+	almostSafeReports := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		report := parseReport(line)
 
+		// part 1
 		if isSafe(report) {
+			// fmt.Printf("report: %v\n", report)
 			safeReports++
+		}
+
+		//part 2
+		if isAlmostSafe(report) || isAlmostSafe(reverse(report)) {
+			// fmt.Printf("report: %v\n", report)
+			almostSafeReports++
 		}
 	}
 
@@ -79,7 +139,7 @@ func main() {
 	fmt.Printf("result 1: %v\n", safeReports)
 
 	// part 2
-	// fmt.Printf("result 2: %v\n", result_2)
+	fmt.Printf("result 2: %v\n", almostSafeReports)
 
 	elapsed := time.Since(start)
 	fmt.Printf("\n\nExecution time: %s\n", elapsed)
