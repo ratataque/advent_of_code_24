26c1598a4a70bcabc2ef01b9ac5767ff6f041101
ratataque <ewanrimmely@outlook.fr>
2024-12-03 13:59:14 +0100

    fixed

--- day_3/main.go
+++ day_3/main.go
@@ -1,32 +1,51 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
-func sumMul(input string) int {
+func sumMul(bytes []byte) int {
 	total := 0
-	bytes := []byte(input)
+	// bytes := []byte(input)
 	// fmt.Printf("bytes: %v\n", bytes)
 
+	disable := false
 	for i := 0; i < len(bytes)-8; i++ {
+		if !disable &&
+			bytes[i] == 'd' &&
+			bytes[i+1] == 'o' &&
+			bytes[i+2] == 'n' &&
+			bytes[i+3] == '\'' &&
+			bytes[i+4] == 't' &&
+			bytes[i+5] == '(' &&
+			bytes[i+6] == ')' {
+			disable = true
+			continue
+
+		}
+
+		if disable &&
+			bytes[i] == 'd' &&
+			bytes[i+1] == 'o' &&
+			bytes[i+2] == '(' &&
+			bytes[i+3] == ')' {
+
+			disable = false
+			continue
+		}
+
 		// Check for 'mul('
-		if bytes[i] == 'm' &&
+		if !disable &&
+			bytes[i] == 'm' &&
 			bytes[i+1] == 'u' &&
 			bytes[i+2] == 'l' &&
 			bytes[i+3] == '(' {
 
-			// Find first number
-			// fmt.Printf("bytes[i+4]: %v\n", []byte("9"))
-
-			// fmt.Printf("bytes[numStart]: %q\n", bytes[numStart])
-			// fmt.Printf("bytes[numStart]: %v\n", bytes[numStart] < '3')
-			// fmt.Printf("bytes[numStart]: %v\n", (bytes[numStart] < '0' || bytes[numStart] > '9'))
 			numStart := i + 4
 			if numStart < len(bytes) && (bytes[numStart] < '0' || bytes[numStart] > '9') {
 				// fmt.Printf("numStart: %v\n", numStart)
@@ -39,7 +58,6 @@ func sumMul(input string) int {
 				num1 = num1*10 + int(bytes[numStart]-'0')
 				numStart++
 			}
-			// fmt.Printf("num2: %v\n", num1)
 
 			// find comma
 			if numStart < len(bytes) && bytes[numStart] != ',' {
@@ -64,8 +82,6 @@ func sumMul(input string) int {
 
 			// check for closing parenthesis
 			if numStart < len(bytes) && bytes[numStart] == ')' {
-				// fmt.Printf("num1: %v\n", num1)
-				// fmt.Printf("num2: %v\n", num2)
 				// println(num1, num2)
 				total += num1 * num2
 			}
@@ -88,29 +104,52 @@ func regSumMul(input_string string) int {
 	return result_1
 }
 
+func regSumMul2(input_string string) int {
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\((-?\d+),(-?\d+)\)`)
+
+	mulEnabled := true
+	total := 0
+
+	matches := re.FindAllStringSubmatch(input_string, -1)
+	// fmt.Printf("matches: %q\n", matches)
+	for _, match := range matches {
+		if match[0] == "do()" {
+			mulEnabled = true
+		} else if match[0] == "don't()" {
+			mulEnabled = false
+		} else if strings.Contains(match[0], "mul") {
+			if mulEnabled {
+				a, _ := strconv.Atoi(match[1])
+				b, _ := strconv.Atoi(match[2])
+				// println(a, b)
+				total += a * b
+			}
+		}
+	}
+	return total
+}
+
 func main() {
 	start := time.Now()
 
-	file, _ := os.Open("day_3/input.txt")
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
+	// file, _ := os.Open("day_3/input.txt")
+	file, _ := os.ReadFile("day_3/input.txt")
+	// input_string := string(file)
 
 	// reg_result_tot_1 := 0
 	bytes_result_tot_1 := 0
-	for scanner.Scan() {
-		input_string := scanner.Text()
 
-		// reg_result_tot_1 += regSumMul(input_string)
+	// fmt.Printf("input_string: %v\n", input_string)
 
-		bytes_result_tot_1 += sumMul(input_string)
-	}
+	// reg_result_tot_1 += regSumMul2(input_string)
+
+	bytes_result_tot_1 += sumMul(file)
 
 	//part 1
 
 	// fmt.Printf("numbers: %#v\n", final_list)
-	// fmt.Printf("result 1: %v\n", reg_result_tot_1)
-	fmt.Printf("result 1: %v\n", bytes_result_tot_1)
+	// fmt.Printf("result regex: %v\n", reg_result_tot_1)
+	fmt.Printf("result bytes: %v\n", bytes_result_tot_1)
 
 	// part 2
 	// fmt.Printf("result 2: %v\n", almostSafeReports)
