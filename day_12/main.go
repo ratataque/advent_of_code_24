26180b50df26cfa524bfc322b6d3b332c2415f03
ratataque <ewanrimmely@outlook.fr>
2024-12-13 02:20:51 +0100

    p1

--- day_12/main.go
+++ day_12/main.go
@@ -12,27 +12,57 @@ func main() {
 	file, _ := os.ReadFile("day_12/input.txt")
 
 	lines := strings.Split(string(file), "\n")
+	lines = lines[:len(lines)-1]
 
-	plants := map[rune][2]int{}
-	for y, line := range lines[:len(lines)-1] {
-		for x, plant := range line {
-			if _, test := plants[plant]; !test {
-				plants[plant] = [2]int{x, y}
+	field := []map[[2]int]bool{}
+	visited := map[[2]int]int{}
+
+	directions := [][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+	for y_ori, line := range lines {
+		for x_ori, plant := range line {
+			if _, existe := visited[[2]int{x_ori, y_ori}]; !existe {
+				visited[[2]int{x_ori, y_ori}] = 4
+
+				queue := [][2]int{{x_ori, y_ori}}
+				region := map[[2]int]bool{{x_ori, y_ori}: true}
+				for len(queue) > 0 {
+					for dir := range directions {
+						x, y := queue[0][0], queue[0][1]
+						new_x := x + directions[dir][0]
+						new_y := y + directions[dir][1]
+
+						if new_x >= 0 && new_x < len(lines[y]) && new_y >= 0 && new_y < len(lines) {
+							if []rune(lines[new_y])[new_x] == plant {
+								if _, test := visited[[2]int{new_x, new_y}]; !test {
+									visited[[2]int{new_x, new_y}] = 4
+									visited[[2]int{x, y}]--
+									region[[2]int{new_x, new_y}] = true
+									queue = append(queue, [2]int{new_x, new_y})
+
+								} else {
+									visited[[2]int{x, y}]--
+								}
+							}
+						}
+					}
+					queue = queue[1:]
+				}
+				field = append(field, region)
 			}
 		}
 	}
 
-	// visited := map[string]map[[2]int]int{}
-	// queue := [][2]int{}
-	// for plant := range plants {
-	// 	for len(queue) > 0 {
-	//
-	// 	}
-	// }
+	total_1 := 0
+	for _, region := range field {
+		fence := 0
+		for plant := range region {
+			fence += visited[plant]
+		}
+		total_1 += fence * len(region)
+	}
 
 	// Part 1
-	fmt.Printf("Part 1: %v\n", plants)
-	// fmt.Printf("Part 1: %v\n", input)
+	fmt.Printf("Part 1: %v\n", total_1)
 
 	// Part 2
 	// fmt.Printf("Part 2: %v\n", total_2)
